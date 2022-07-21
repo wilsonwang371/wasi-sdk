3e6e6a2b68d6bc8d4c35c349da60df87cc2221a4
Wilson Wang <3913185+wilsonwang371@users.noreply.github.com>
2022-07-20 23:32:12 -0700

    Update docker_build.sh

--- docker_build.sh
+++ docker_build.sh
@@ -4,4 +4,4 @@ echo "Building the docker"
 docker build -t wasi-sdk-builder:latest .
 echo "Building the package in docker"
 # docker run --mount type=bind,src=$PWD,target=/workspace -e NINJA_FLAGS=-v --workdir /workspace wasi-sdk-builder:latest make package
-docker run -v $(pwd -P):/opt/wasi-sdk -e NINJA_FLAGS=-v wasi-sdk-builder:latest make package
+docker run -v $(pwd -P):/opt/wasi-sdk -e NINJA_FLAGS=-v wasi-sdk-builder:latest /bin/bash -c "cd /opt/wasi-sdk; make package"
